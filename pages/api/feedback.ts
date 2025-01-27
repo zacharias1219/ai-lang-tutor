39f46da1fe3c3a4ecfa35143e529d4e8dd4f59e1
Richard Abishai <rickoshade1891@gmail.com>
2025-01-27 20:12:03 +0530

    Add API endpoints for chat and feedback functionalities in the AI Language Agent application

---
+++ pages/api/feedback.ts
@@ -0,0 +1,31 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Ollama } from 'ollama'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+    
+    const conversation = [
+        { 
+            role: "system",
+            content: `You are an expert english teacher. You correct students mistakes by giving valuable and insightful feedbacks. 
+            Only give the review and no other content.
+            ` 
+        },
+        { 
+            role: "assistant",
+            content: `Which content can I review?` 
+        },
+        { 
+            role: "user",
+            content: `Review this english text: ${req.body.message} ` 
+        }
+    ]
+    const ollama = new Ollama({host: "http://localhost:11434"})
+    const message = await ollama.chat({
+        model: "gemma:2b",
+        messages: conversation
+    })
+    res.status(200).json({ response: message })
+}
